9b1d5a98f3d41c1b2d75da5a9700c4bf25f1cb42
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-10-10 14:11:41 +0530

    date validator

--- dateValidator.py
+++ dateValidator.py
@@ -1,16 +1,16 @@
 import re
 
-def checkDateFormat(username):
-    regex = "^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$"
+def checkDateFormat(date):
+    regex = "^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$"
     r = re.compile(regex)
 
-    if(re.search(r, username)):
+    if(re.search(r, date)):
         print("Valid")
     else:
         print("Not Valid")
 
-date1 = "abc@gmail.com"
+date1 = "03/21/2002"
 checkDateFormat(date1)
 
-date2 = "abc@def@gmail.kahscg"
+date2 = "21-03-2002"
 checkDateFormat(date2)
