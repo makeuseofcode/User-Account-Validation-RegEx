6e02b95e0caf3a088380de4971939b0a23f5e81e
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-10-10 18:46:39 +0530

    updated regecx for empty string

--- emptyString.py
+++ emptyString.py
@@ -1,15 +1,15 @@
 import re
 
 def checkEmptyString(str):
-    regex = "^\s*$"
+    regex = "^$"
     r = re.compile(regex)
 
     if(re.search(r, str)):
-        print("The given string is empty and it only contains whitespaces")
+        print("The given string is empty")
     else:
         print("The given string is not empty")
 
-str1 = "              "
+str1 = ""
 checkEmptyString(str1)
 
 str2 = "This is not an empty string"
